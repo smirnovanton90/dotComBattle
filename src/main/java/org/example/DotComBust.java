626f89d5c6227a46c840763676de40bc72254495
smirnovanton90 <smirnovanton90@gmail.com>
2022-11-07 22:28:56 +0300

    Implemented checkUserGuess, finishGame, main

--- src/main/java/org/example/DotComBust.java
+++ src/main/java/org/example/DotComBust.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 public class DotComBust {
 
     //VARIABLES
-    private ArrayList<Character> rows = new ArrayList<>();
-    private ArrayList<Integer> collumns = new ArrayList<>();
+    //TODO сделать проверку на попадание в обозначенный периметр
+//    private ArrayList<Character> rows = new ArrayList<>();
+//    private ArrayList<Integer> collumns = new ArrayList<>();
     private ArrayList<DotCom> dotComsList = new ArrayList<>();
     private GameHelper helper = new GameHelper();
+    private int numOfGuesses = 0;
 
     //SETTERS
-    public void setRows(ArrayList<Character> rows) {
-        this.rows = rows;
-    }
-    public void setCollumns(ArrayList<Integer> collumns) {
-        this.collumns = collumns;
-    }
+//    public void setRows(ArrayList<Character> rows) {
+//        this.rows = rows;
+//    }
+//    public void setCollumns(ArrayList<Integer> collumns) {
+//        this.collumns = collumns;
+//    }
 
     //METHODS
     private void setUpGame() {
@@ -46,14 +48,34 @@ public class DotComBust {
         finishGame();
     }
     private void checkUserGuess (String userGuess) {
-
+        numOfGuesses++;
+        String result = "Мимо";
+        for (DotCom dotComToTest : dotComsList) {
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("Попал")) { //TODO сделать не текс, а ссылки
+                break;
+            }
+            if (result.equals("Потопил")) {
+                dotComsList.remove(dotComToTest);
+            }
+        }
+        System.out.println(result);
     }
     private void finishGame () {
-
+        System.out.println("Все сайты ушли ко дну! Ваши акции теперь ничего не стоят.");
+        if (numOfGuesses <= 18) {
+            System.out.println("Это заняло у вас всего " + numOfGuesses + " попыток.");
+            System.out.println("Вы успели выбраться до того, как ваши вложения утонули.");
+        } else {
+            System.out.println("Это заняло у вас довольно много времени. " + numOfGuesses + " попыток.");
+            System.out.println("Рыбы водят хороводы вокруг ваших вложений.");
+        }
     }
 
     public static void main(String[] args) {
-
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
     }
 
 
